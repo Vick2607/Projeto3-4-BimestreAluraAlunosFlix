408a01ceb15b0b3872a2ccc4b1583c16cdbc0856
Vick2607 <00001105245925sp@aluno.educacao.sp.gov.br>
2024-11-26 09:32:54 -0300

    Add files via upload

---
+++ perguntas.jv.py
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'O algarismo é um conjunto de instruções, como uma receita de bolo, constituído de um número finito de passos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o único continente com terra nos quatro hemisférios?',
+    'A África é o único continente situado em todos os hemisférios da terra: norte, sul, leste e oeste'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o último continente a ser povoado pelos seres humanos?',
+    'A América'
+)
+
+criaCartao(
+    'Lingua francesa',
+    'Como se diz bom dia em Francês?',
+    'Bom dia em franês é BONJOUR'
+)
